6f4d3b8937b6ab7f21683ddd05210ef75e6d1a9f
spelapovrzenic <spela.povrzenic@gmail.com>
2016-05-05 09:09:25 +0200

    dodala csv Kontinenti.csv uvozila Tabele naredila nov dokument xml.R iz kjer bomo uvozili linke

--- Tabele - uvoz/Uvoz.R
+++ Tabele - uvoz/Uvoz.R
@@ -1,16 +1,29 @@
 # Uvozimo potrebne knji≈ænice
 library(rvest)
-#library(dplyr) ta package ne deluje
+#library(dplyr) 
 library(gsubfn)
 
-#Prvi link
+# LINKI
+#Prvi link - januar -junij **
 link <- "https://en.wikipedia.org/wiki/List_of_terrorist_incidents,_January%E2%80%93June_2015"
 stran <- html_session(link) %>% read_html()
-#Drugi link
+#Drugi link - julij - december**
 link2 <- "https://en.wikipedia.org/wiki/List_of_terrorist_incidents,_July%E2%80%93December_2015"
 stran2 <- html_session(link2) %>% read_html()
+#3.link religije ****
+link3 <- "https://en.wikipedia.org/wiki/Religions_by_country"
+stran3 <- html_session(link3) %>% read_html()
 
-# Shranimo tabele
+
+#4.link drûave - glavna mesta
+link4 <- "http://www.go4quiz.com/1023/lworld-countries-and-their-capitals/"
+stran4 <- html_session(link4) %>% read_html()
+
+
+
+###########################################################################
+# TABELE wiki
+#MESECI
 januar <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
   .[[1]] %>% html_table()
 februar <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
@@ -35,3 +48,25 @@ november <- stran2 %>% html_nodes(xpath="//table[@class='wikitable sortable']") 
   .[[5]] %>% html_table()
 december <- stran2 %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
   .[[6]] %>% html_table()
+
+#RELIGIJE
+religije <- stran3 %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
+  .[[1]] %>% html_table()
+
+
+#######################################################################################################
+#AREA in POPULATION
+source("Tabele - uvoz/xml.r", encoding="UTF-8")
+cat("Uvaûam podatke")
+area <- uvozi.area()
+
+#KONTINENTI
+uvozi1 <- function() {
+  return(read.csv("Tabele - uvoz/Kontinenti.csv", sep = ";", as.is = TRUE,
+                  row.names = 1, na.strings=c("-", "z") ,
+                  fileEncoding = "Windows-1250"))
+}
+
+cat("Uvaûam podatke ")
+kontinenti <- uvozi1()
+
