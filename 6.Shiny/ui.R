81ef34895a50a23b2235515f29634a32cfafbe3e
Špela Povrženič <spela.povrzenic@gmail.com>
2016-05-31 23:27:08 +0200

    sprememba

--- 6.Shiny/ui.R
+++ 6.Shiny/ui.R
@@ -6,16 +6,16 @@ shinyUI(fluidPage(
   #APLIKACIJA 2
   
   #SEZNAM NAPADOV IN NJIHOVE LASTNOSTI, GLEDE NA VRSTO CELINE, RELIGIJE, Ali glavno mesto napadeno
-  titlePanel('NAPADI'),
+  titlePanel('SEZNAM TERORISTIČNIH NAPADOV v letu 2015'),
   sidebarLayout(
     sidebarPanel(
       uiOutput("kontinent"),
       uiOutput("datum"),
       uiOutput("religije1"),
-      selectInput("mesec", "Choose month:",
-                  choices=c("All" = 0, setNames(1:12, month.name))),
+      #selectInput("mesec", "Choose month:",
+       #           choices=c("All" = 0, setNames(1:12, month.name))),
       selectInput("gl.mesto", "Pokaži SAMO napade, ki so se zgodili v glavnem mestu:",
-                  choices = c("Da" = TRUE, "Ne" = FALSE))
+                  choices = c("Da, samo v glavnih mestih" = TRUE, "Ne, pokaži vse napade" = FALSE))
       #uiOutput("glmesto"),
     ),
     mainPanel(
