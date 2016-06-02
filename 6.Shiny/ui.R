750c8ae40b754f0c1f8617c40842aca8232e1a16
Špela Povrženič <spela.povrzenic@gmail.com>
2016-06-02 17:56:55 +0200

    sprememba

--- 6.Shiny/ui.R
+++ 6.Shiny/ui.R
@@ -3,28 +3,9 @@
 shinyUI(fluidPage(
   
   #########################################################
-  #APLIKACIJA 2
-  
-  #SEZNAM NAPADOV IN NJIHOVE LASTNOSTI, GLEDE NA VRSTO CELINE, RELIGIJE, Ali glavno mesto napadeno
-#   titlePanel('List of terrorist attacks in 2015'),
-#   sidebarLayout(
-#     sidebarPanel(
-#       uiOutput("kontinent"),
-#       uiOutput("datum"),
-#       uiOutput("religije1"),
-#       #selectInput("mesec", "Choose a month:",
-#        #           choices=c("All" = 0, setNames(1:12, month.name))),
-#       selectInput("gl.mesto", "Show attacks that happened ONLY in the capital:",
-#                   choices = c("No, show all attacks" = FALSE, "Yes, show attacks in capitals" = TRUE))
-#       #uiOutput("glmesto"),
-#     ),
-#     mainPanel(
-#       tableOutput('napadi1')
-#     )
-#   )
-  ####################################################################
 
-titlePanel('Religije'),
+titlePanel('Religije1'),
+ tabsetPanel(
 sidebarLayout(
   sidebarPanel(
     uiOutput("kontinent"),
@@ -33,7 +14,17 @@ sidebarLayout(
   mainPanel(
     plotOutput('religionPlot1')
   )
-)
+)),
+ tabsetPanel(
+sidebarLayout(
+  sidebarPanel(
+    uiOutput("kontinentA"),
+    selectInput("mesec", "Choose a month:",choices=c("All" = 0, setNames(1:12, month.name)))
+  ),
+  mainPanel(
+    plotOutput('religionPlot2')
+  )
+))
   
   
   #####################################################################
